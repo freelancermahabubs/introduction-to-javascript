ec6f2bcf91fa953544214e8fef72570d7739c919
MAHABUB <info.freelancermahabub@gmail.com>
2023-01-25 22:09:13 +0600

    explore js

---
+++ variable.js
@@ -0,0 +1,18 @@
+// vary + able = variable
+// numeric data types
+var price = 13;
+var age = 21;
+var temperature = 22;
+var weight = 120;
+var money = 1500;
+var computer = 55;
+
+// string types data
+var name = "mahabub alom" ;
+var location = "netrokona city" ;
+var special = 'mohammud (sm)' ;
+
+// Boolean Data types
+var serious = true;
+var isFullMarks = true;
+var isSingle = true;
